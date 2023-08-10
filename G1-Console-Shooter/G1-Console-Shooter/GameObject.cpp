77c804facece9e1eb67151faac5d9a2ffe041642
Joshua Hu <joshua.hu@mds.ac.nz>
2023-08-10 14:46:15 +1200

    GameObject Functions Updated
    Added functionality for Initialisation and ProcessInput. Also added #include <iostream> to Controller.h

--- G1-Console-Shooter/G1-Console-Shooter/GameObject.cpp
+++ G1-Console-Shooter/G1-Console-Shooter/GameObject.cpp
@@ -6,6 +6,9 @@ CGameObject::CGameObject()
 
 void CGameObject::Initialisation(int _iPositionX, int _iPositionY, char _cToken)
 {
+	m_iPositionX = _iPositionX;
+	m_iPositionY = _iPositionY;
+	m_cToken = _cToken;
 }
 
 void CGameObject::ProcessInput(MovementInput& _Input)
@@ -18,4 +21,6 @@ void CGameObject::ProcessUpdate()
 
 void CGameObject::ProcessDraw(Controller& _controller)
 {
+	_controller.SetConsoleCursor(m_iPositionX, m_iPositionY);
+	std::cout << m_cToken;
 }
